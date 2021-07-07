c03a7a9a013ab5996ef82d60278f173105b11da3
unknown <stevenchristpa2001@gmail.com>
2021-07-07 20:54:42 +0700

    bug pendaftaran

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -61,7 +61,7 @@ class AdminController extends Controller
     public function indexPendaftaran(){
         $akun=Auth::user();
         $laporan = Registrasi::with(['status'])->get();
-        $status=Status::where('id_status','!=','1');
+        $status=Status::where('id_status','!=','1')->get();
         return view("Admin.laporan.pendaftaran.index", ['laporan' => $laporan,'akun' => $akun, 'list_status' => $status]);
     }
 
